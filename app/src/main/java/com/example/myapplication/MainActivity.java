85c460f464ee9ef7a2335e2a81c36e575576e709
bglow <b.glowacki123456@gmail.com>
2025-05-08 16:47:42 +0200

    mala poprawka

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,50 +13,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private int coun = 0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        int coun = 0;
+        textView = findViewById(R.id.countdel);
+        findViewById(R.id.delete).setOnClickListener(v -> showQueryRequest());
+    }
 
-        TextView textView = findViewById(R.id.countdel);
-        findViewById(R.id.delete).setOnClickListener(showQueryRequest());
+    private void showQueryRequest() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Uwaga");
+        builder.setMessage("Czy na pewno chcesz usunąć dane?");
+        builder.setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                coun += 1;
+                Toast.makeText(MainActivity.this, "Dane zostały usunięte", Toast.LENGTH_SHORT).show();
+                textView.setText("");
+            }
+        });
 
-        }
-        private View.OnClickListener showQueryRequest(){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Uwaga");
-            builder.setMessage("Czy na pewno chcesz usunąć dane?");
-            builder.setPositiveButton("Tak", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    int coun += 1;
-                    Toast.makeText(MainActivity.this, "Dane zostały usunięte", Toast.LENGTH_SHORT).show();
-                    textView.setText("")
-                }
-            });
+        builder.setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Brak zmian", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-            builder.setNegativeButton("Nie", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    Toast.makeText(MainActivity.this, "Brak zmian", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            builder.create().show();
-            return null;
-        }
-
-
-
+        builder.create().show();
+    }
 }
